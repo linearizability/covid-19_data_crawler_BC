5265630afd1eb9ced77b438034e6fc7453db6837
ZhangBoyuan <byuan98@outlook.com>
2024-05-03 21:56:03 +0800

    feat: edit crawler main demo

--- boot/main.py
+++ boot/main.py
@@ -4,47 +4,30 @@
 # @File main.py
 # @Author : ZhangBoyuan
 # @Time : 2024/1/6 下午11:10
+import json
 import sqlite3
 
-import bs4
 import requests
 
 from constant.url_path import UrlPath
 
 
-def get_covid19_content_url_list():
-    result = []
-
+def get_covid19_data_dict():
     response = requests.get(url=UrlPath.INDEX_URL)
     response.encoding = "utf-8"
-    bs = bs4.BeautifulSoup(response.text, "html.parser")
-
-    for a_label in bs.find_all("a"):
-        if "新型冠状病毒肺炎疫情最新情况" in a_label.get_text():
-            result.append("https://www.gov.cn" + a_label.get("href"))
-
-    return result
+    dataJson = json.loads(response.text);
+    return dataJson['value']
 
 
 if __name__ == '__main__':
-    covid19_content_url_list = get_covid19_content_url_list()
+    covid19DataDict = get_covid19_data_dict()
 
     conn = sqlite3.connect("../static/covid-19.db")
     cur = conn.cursor()
 
-    for covid19_content_url in covid19_content_url_list:
-        response = requests.get(url=covid19_content_url)
-        response.encoding = "utf-8"
-        bs = bs4.BeautifulSoup(response.text, "html.parser")
-        url_content = bs.find(id="UCAP-CONTENT").find_all("p")
-
-        title = bs.title.string
-        content = ""
-
-        for i in url_content:
-            content = content + i.get_text()
-
-        sql = """insert into data_source values ('%s', '%s', '%s');""" % (covid19_content_url, title, content)
+    for i in covid19DataDict:
+        sql = """insert into covid_19_data values ('%s', '%s', '%s');""" % (
+        i.get('DIM_TIME'), i.get('DIM_GEO_CODE_M49'), str(i.get('VALUE_NUMERIC')))
 
         cur.execute(sql)
         conn.commit()
