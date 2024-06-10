f7fd32c8445b72eadf1435c5e9d72d90a991ffa7
ZhangBoyuan <byuan98@outlook.com>
2024-06-10 18:30:08 +0800

    feat: add CollectionUtils

---
+++ utils/CollectionUtils.py
@@ -0,0 +1,19 @@
+# -*- coding = utf-8 -*-
+# @Software: PyCharm
+# @File CollectionUtils.py
+# @Author : ZhangBoyuan
+# @Time : 2024/6/10 18:14
+
+
+def isEmpty(collection):
+    if collection is None:
+        return True
+    if not isinstance(collection, list):
+        return False
+    if len(collection) <= 0:
+        return True
+    return False
+
+
+def isNotEmpty(collection):
+    return not isEmpty(collection)
