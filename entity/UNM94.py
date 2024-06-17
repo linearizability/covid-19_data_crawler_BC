673c900a6293683117b80adbe6e64992d1b3a90a
ZhiFei <byuan98@outlook.com>
2024-06-17 18:57:23 +0800

    feat: add UNM94 entity class

---
+++ entity/UNM94.py
@@ -0,0 +1,25 @@
+# -*- coding = utf-8 -*-
+# @Software: PyCharm
+# @File UNM94.py
+# @Author : ZhangBoyuan
+# @Time : 2024/6/17 18:53
+
+from sqlalchemy import Column, String, create_engine, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class UNM94(Base):
+    __tablename__ = 'UNM94'
+    ID = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    COUNTRY_NAME = Column(String(255))
+
+    def __init__(self, COUNTRY_NAME):
+        self.COUNTRY_NAME = COUNTRY_NAME
+
+
+engine = create_engine("sqlite:///../static/covid-19.db")
+
+DBSession = sessionmaker(bind=engine)
