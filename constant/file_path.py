224a274d2fc730fb2333bf4e6dcc935af1535cc7
ZhiFei <byuan98@outlook.com>
2025-06-24 10:33:21 +0800

    feat: integration scrapy, demo init

--- constant/file_path.py
+++
@@ -1,6 +0,0 @@
-# -*- coding = utf-8 -*-
-# 文件路径常量
-# @Software: PyCharm
-# @File file_path.py
-# @Author : ZhangBoyuan
-# @Time : 2024/1/6 下午11:24
---
+++ main.py
@@ -0,0 +1,8 @@
+# -*- coding = utf-8 -*-
+# @Software: PyCharm
+# @File main.py
+# @Author : ZhangBoyuan
+# @Time : 2025/6/24 10:25
+from scrapy.cmdline import execute
+
+execute('scrapy crawl quotes -o quotes.json'.split())
